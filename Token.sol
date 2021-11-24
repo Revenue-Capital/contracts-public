04fc6455d6c90180a3f09a1c03f26a425231df2f
kubakorecki <kubakorecki@gmail.com>
2021-11-24 12:47:52 +0100

    add AntiBot to Token

--- Token.sol
+++ Token.sol
@@ -4,12 +4,13 @@ pragma solidity =0.8.6;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "./AntiBot.sol";
 
 /**
  * @title Token
  * @dev BEP20 compatible token.
  */
-contract Token is ERC20Burnable, Ownable {
+contract Token is ERC20Burnable, Ownable, AntiBot {
 
     /**
      * @dev Mints all tokens to deployer
@@ -19,5 +20,20 @@ contract Token is ERC20Burnable, Ownable {
      */
     constructor(uint256 amount, string memory name, string memory symbol) ERC20(name, symbol) {
         _mint(_msgSender(), amount);
+    }
+
+    /**
+     * @dev Returns the address of the current owner.
+     *
+     * IMPORTANT: This method is required to be able to transfer tokens directly between their Binance Chain
+     * and Binance Smart Chain. More on this issue can be found in:
+     * https://github.com/binance-chain/BEPs/blob/master/BEP20.md#5116-getowner
+     */
+    function getOwner() external view returns (address) {
+        return owner();
+    }
+
+    function _transfer(address sender, address recipient, uint256 amount) internal virtual override transferThrottler(sender, recipient, amount) {
+        super._transfer(sender, recipient, amount);
     }
 }
